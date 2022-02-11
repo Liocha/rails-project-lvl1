3369472bcf7055d2e4ac5cc8b3a0d5a7551c859c
Alex <dev@liocha.ru>
2022-02-11 08:43:14 +0300

    fix bug

--- lib/hexlet_code/inputs/text_input.rb
+++ lib/hexlet_code/inputs/text_input.rb
@@ -4,8 +4,13 @@ module HexletCode
   module Inputs
     # top-level documentation comment for class HexletCode::Inputs::TextInput
     class TextInput < Base
+      def initialize(element)
+        super
+        @label = "<label for='#{@atr_name}'>#{@atr_name.capitalize}</label>\n"
+      end
+
       def render
-        "<textarea #{@atributes} name='#{@atr_name}'>#{@value}</textarea>\n"
+        "#{@label}<textarea #{@atributes} name='#{@atr_name}'>#{@value}</textarea>\n"
       end
     end
   end
